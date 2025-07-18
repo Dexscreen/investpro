7c36982e567031cfacd27fa5362111c35ff693c5
Dexscreen <paulbirdseyevision1@gmail.com>
2025-07-18 10:40:27 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+// App.tsx
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -15,24 +16,22 @@ import Onboarding from './pages/Onboarding';
 function App() {
   return (
     <HelmetProvider>
-      <Router>
-        <div className="min-h-screen bg-white">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/investment-advisors" element={<InvestmentAdvisors />} />
-              <Route path="/crypto-trading" element={<CryptoTrading />} />
-              <Route path="/online-brokerage" element={<OnlineBrokerage />} />
-              <Route path="/crypto-mining" element={<CryptoMining />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:slug" element={<BlogPost />} />
-              <Route path="/onboarding" element={<Onboarding />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <div className="min-h-screen bg-white">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/investment-advisors" element={<InvestmentAdvisors />} />
+            <Route path="/crypto-trading" element={<CryptoTrading />} />
+            <Route path="/online-brokerage" element={<OnlineBrokerage />} />
+            <Route path="/crypto-mining" element={<CryptoMining />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<BlogPost />} />
+            <Route path="/onboarding" element={<Onboarding />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </HelmetProvider>
   );
 }
