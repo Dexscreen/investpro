2dcfbf3fa2e315869721df40c2db1b8126a3c3e6
Dexscreen <paulbirdseyevision1@gmail.com>
2025-07-18 10:24:18 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // <-- import HashRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>             {/* Wrap App in HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
